042b4c59c80dbb91bc9f3c136328abe155e68f22
Andres Roa <andresmore1423@hotmail.com>
2023-01-09 10:41:06 -0500

    finalized bot fotos

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const mongoose = require("mongoose")
 
+const { Clientes } = require("./src/models/index.js")
+
 // inicializar la instacia de express
 const app = express();
 
@@ -24,7 +26,7 @@ const client = new Client({
     },
     authStrategy: new LocalAuth({
     clientId: 'client-oasis',
-    dataPath: './session.json'
+    dataPath: './session'
 })})
 
 //generacion del qr
@@ -38,6 +40,61 @@ client.on('authenticated', () => {
     console.log('Sesión detectada e iniciada')
 });
 
+client.on('message', async (message) => {
+    if(message.body.toLocaleLowerCase() == 'hola' 
+    || message.body.toLocaleLowerCase() == 'holaa'
+    || message.body.toLocaleLowerCase() == 'holi'
+    || message.body.toLocaleLowerCase() == 'hola.'
+    || message.body.toLocaleLowerCase() == 'hola, buenos días'
+    || message.body.toLocaleLowerCase() == 'hola buenos días'
+    || message.body.toLocaleLowerCase() == 'hola, buenos dias'
+    || message.body.toLocaleLowerCase() == 'hola buenos dias'
+    || message.body.toLocaleLowerCase() == 'hola, buenas noches'
+    || message.body.toLocaleLowerCase() == 'hola buenas noches'
+    || message.body.toLocaleLowerCase() == 'hola, buenas tardes'
+    || message.body.toLocaleLowerCase() == 'hola buenas tardes'
+    ) {
+        client.sendMessage(message.from, `Hola ${message._data.notifyName}, un gusto saludarte, mi nombre es Sofía, seré tu chat bot de ayuda para reclamar tus fotografías.\n\n¿Quieres saber el estado de tus fotografías?\n_Escribe la respuesta como aparece en este mensaje por favor_\n1) Si\n2) No\n\nEspero poderte ser de ayuda`)
+    }
+    if(message.body.toLocaleLowerCase() == 'si'
+    || message.body.toLocaleLowerCase() == 'sí') {
+        client.sendMessage(message.from, `Genial!! ${message._data.notifyName}, para ayudarte por favor digita el número que registrarte al momento de tomarte las fotografías por favor\n\n*Sin espacios, y tampoco signos especiales*`)
+    }
+    if(message.body.trim().length == 10) {
+        try {
+            const datos = await Clientes.findOne({telefono: {$eq: message.body.trim()}})
+            if(!datos) {
+                client.sendMessage(message.from, 'Lo sentimos, no tenemos registro de este número. \n\nVerifica que el número este bien escrito, o solicita hablar con un asesor escribiendo: *-asesor*')
+                return
+            }
+            client.sendMessage(message.from, `Excelentes noticias ${message._data.notifyName} Hemos encontrado un match con ese número`)
+            client.sendMessage(message.from, `Cliente: *${datos.fullName}*\n\nEstado actual de tu orden: *${datos.estado ? 'Fotografías envíadas' : 'En proceso de edicion'}*\n\nPaquete adquirido: *${datos.paquete}*`)
+            datos.estado ? client.sendMessage(message.from, '¿No has recibido tus fotografías?\n\nEscribe el siguiente mensaje: *-reenviar (número del celular registrado)*') : null
+            return
+        } catch (error) {
+            client.sendMessage(message.from, 'Lo sentimos, nuestro servidor está fallando. Intentalo más tarde')
+        }
+    }
+    if(message.body.toLocaleLowerCase().split(' ')[0] == '-reenviar') {
+        try {
+            const datos = await Clientes.findOne({telefono: {$eq: message.body.split(' ')[1]}})
+            client.sendMessage(`57${datos.telefono}@c.us`, `Gracias por tu espera, puedes descargar tus fotografías en el siguiente enlace:\n\n${datos.link}\n\nSino te aparece el link en azul, envianos cualquier mensaje para que whatsapp no nos detecte el chat como spam y habilite el link.\n\n*NOTA:* El link estará habilidato por 7 días, no olvides descargarlas lo antes posible\n\n`)
+            client.sendMessage(message.from, `${message._data.notifyName} hemos reenviado tus fotos correctamente`)
+            return
+        } catch (error) {
+            client.sendMessage(message.from, 'Lo sentimos, nuestro servidor está fallando. Intentalo más tarde')
+        }
+    }
+    if(message.body.toLocaleLowerCase() == 'no') {
+        client.sendMessage(message.from, '*_Gracias por contactarnos, recuerda que somos Oasis Eco Park_*')
+    }
+    if(message.body.toLocaleLowerCase() == '-asesor') {
+        client.sendMessage(message.from, 'Genial, por el momento tenemos dos agentes en línea: soyAndres o soyLaura, escribe el nombre del asesor que quieres que te contacte')
+    }
+    if(message.body.toLocaleLowerCase() == 'soylaura') client.sendMessage('573108098741@c.us', `Laura, el número ${message.from} está solicitando hablar contigo`)
+    if(message.body.toLocaleLowerCase() == 'soyandres') client.sendMessage('573227923083@c.us', `Andres, el número ${message.from} está solicitando hablar contigo`)
+})
+
 client.initialize();
 
 // importacion de rutas
@@ -49,7 +106,7 @@ dbConect()
 
 //TODO: Crear los demas endpoints de la aplicación
 // inicializacion de los endpoints
-const { Clientes } = require("./src/models/index.js")
+
 
 app.use('/send/sendmessage', async (req, res) => {
     const { message, phone } = req.body;
